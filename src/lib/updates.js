5e987bedb40d713580de77f721e77f79a53d1b5b
n0step_ <krushnavyas06@gmail.com>
2024-10-06 07:41:10 +0000

    revert: remove unuse imports

--- src/lib/updates.js
+++ src/lib/updates.js
@@ -1,7 +1,3 @@
-const { short } = require('leeks.js');
-const ExtendedEmbedBuilder = require('./embed');
-const { version: currentVersion } = require('../../package.json');
-
 /** @param {import("client")} client */
 module.exports = client => {
 	client.log.info('You are running the latest version.');
