1b5c1d3a5017d7c9386ab399c0e27c78566c3e7f
krushna06 <krushnavyas06@gmail.com>
2025-04-18 20:52:43 +0530

    feat: pre-defined close request options

--- src/commands/slash/close.js
+++ src/commands/slash/close.js
@@ -16,6 +16,40 @@ module.exports = class CloseSlashCommand extends SlashCommand {
 					name: 'reason',
 					required: false,
 					type: ApplicationCommandOptionType.String,
+					choices: [
+						{
+							name: 'Ticket Answered',
+							value: 'Ticket answered. Read transcript for details.'
+						},
+						{
+							name: 'Action Taken',
+							value: 'Action Taken! Thank you for contacting Fusion Network.'
+						},
+						{
+							name: 'Issue Resolved',
+							value: 'Issue resolved! Thank you for contacting Fusion Network.'
+						},
+						{
+							name: 'Report Reviewed (Steps Taken)',
+							value: 'Report reviewed and necessary steps taken. Thank you for your help.'
+						},
+						{
+							name: 'Report Reviewed (Community Safety)',
+							value: 'Report reviewed. Steps taken. Thanks for keeping the community safe.'
+						},
+						{
+							name: 'Bug Report Reviewed',
+							value: 'Bug noted. Team will fix soon. Thanks for helping improve Fusion Network.'
+						},
+						{
+							name: 'Appeal Accepted',
+							value: 'Appeal accepted. Follow the rules moving forward. Welcome back!'
+						},
+						{
+							name: 'Appeal Denied',
+							value: 'Appeal denied. Punishment remains due to evidence/past history.'
+						}
+					]					
 				},
 			].map(option => {
 				option.descriptionLocalizations = client.i18n.getAllMessages(`commands.slash.${name}.options.${option.name}.description`);
